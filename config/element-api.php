84ddefca7853a9104e602b36e32dbaf1768e0eb4
Zikisa Cale Sogaxa <zikisacalesogaxa@gmail.com>
2020-07-22 11:47:48 +0200

    some variable name changes...

--- config/element-api.php
+++ config/element-api.php
@@ -8,18 +8,18 @@ return [
 		'news.json' => function() {
 			return [
 				'elementType'	=> Entry::class,
-				'criteria'		=> ['section' => 'news'],
+				'criteria'		=> ['section' => 'newsArticles'],
 				'transformer'	=> function(Entry $entry) {
 					return [
 						'title'		=> $entry->title,
 						'url'		=> $entry->url,
-						'jsonUrl'	=> UrlHelper::url("blog/{$entry->id}.json"),
+						'jsonUrl'	=> UrlHelper::url("news/{$entry->id}.json"),
 						'excerpt'	=> $entry->excerpt,
 					];
 				},
 			];
 		},
-		'blog/<entryid:\d+>.json' => function($entryId) {
+		'news/<entryid:\d+>.json' => function($entryId) {
 			return [
 				'elementType'	=> Entry::class,
 				'criteria'		=> ['id' => $entryId],
